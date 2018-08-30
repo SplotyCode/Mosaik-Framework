2bce2280fdb4ec4e67dc5645eb2e1baa146ed7ef
david <davidscandurra@gmail.com>
2018-08-30 20:59:00 +0200

    Added MongoDB Connection

--- Database/src/main/java/me/david/davidlib/database/connection/impl/MongoConnection.java
+++ Database/src/main/java/me/david/davidlib/database/connection/impl/MongoConnection.java
@@ -1,37 +1,67 @@
 package me.david.davidlib.database.connection.impl;
 
-import com.mongodb.MongoClient;
+import com.mongodb.*;
+import com.mongodb.client.MongoDatabase;
 import lombok.Getter;
 import me.david.davidlib.database.connection.AbstractConnection;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MongoConnection extends AbstractConnection {
 
-    private MongoClient client;
+    @Getter private MongoClient client;
+    @Getter private MongoDatabase database;
     @Getter private boolean connected;
 
     @Override
     public void disconnect() {
-
+        if (client != null)
+            client.close();
+        connected = false;
     }
 
     @Override
     public AbstractConnection connect(String string) {
-        return null;
+        disconnect();
+        MongoClientURI uri = new MongoClientURI(string);
+        client = new MongoClient(uri);
+        database = client.getDatabase(uri.getDatabase());
+        connected = true;
+        return this;
     }
 
     @Override
     public AbstractConnection connect(String host, String user, String password, String database, int port) {
-        return null;
+        MongoCredential credential = MongoCredential.createCredential(user, database, password.toCharArray());
+        MongoClientOptions options = MongoClientOptions.builder().sslEnabled(true).build();
+        client = new MongoClient(new ServerAddress(host, port), Collections.singletonList(credential), options);
+        this.database = client.getDatabase(database);
+        connected = true;
+        return this;
     }
 
     @Override
     public AbstractConnection connect(String host, String database, int port) {
+        client = new MongoClient(host, port);
+        this.database = client.getDatabase(database);
+        connected = true;
         return null;
+    }
+
+    public MongoConnection setDatabase(MongoDatabase database) {
+        this.database = database;
+        return this;
+    }
+
+    public MongoConnection setClient(MongoClient client) {
+        this.client = client;
+        return this;
     }
 
     @Override
     public int getDefaultPort() {
-        return 0;
+        return 27017;
     }
 
 }
