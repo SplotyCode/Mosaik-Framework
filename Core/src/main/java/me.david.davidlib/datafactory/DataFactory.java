16d4cd80cdfc1085114094bf07e3de077b9f7535
david <davidscandurra@gmail.com>
2018-11-04 17:54:07 +0100

    [Core] Added DataFactorys

---
+++ Core/src/main/java/me.david.davidlib/datafactory/DataFactory.java
@@ -0,0 +1,32 @@
+package me.david.davidlib.datafactory;
+
+import lombok.EqualsAndHashCode;
+
+import java.util.HashMap;
+
+@EqualsAndHashCode
+public class DataFactory {
+
+    private HashMap<String, Object> data = new HashMap<>();
+
+    public <T> T getData(DataKey<T> key) {
+        return (T) data.get(key.name);
+    }
+
+    /*
+     * This will use the name instead of the key name
+     * The Key is only for providing the Generic
+     */
+    public <T> T getData(String name, DataKey<T> key) {
+        return (T) data.get(name);
+    }
+
+    public <T> void putData(DataKey<T> key, T obj) {
+        data.putIfAbsent(key.name, obj);
+    }
+
+    public <T> void putData(String name, DataKey<T> key, T obj) {
+        data.putIfAbsent(name, obj);
+    }
+
+}
---
+++ Core/src/main/java/me.david.davidlib/datafactory/DataKey.java
@@ -0,0 +1,12 @@
+package me.david.davidlib.datafactory;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class DataKey<T> {
+
+    protected String name;
+
+}
