6eefb4642ffc7a0f71f8eaf5dc679e7d822001d6
david <davidscandurra@gmail.com>
2018-11-07 19:38:15 +0100

    [WebApi] Added Multithreading for CachedStaticFileContent

---
+++ WebApi/src/main/java/me/david/webapi/response/content/CachedStaticFileContent.java
@@ -0,0 +1,31 @@
+package me.david.webapi.response.content;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CachedStaticFileContent implements ResponseContent {
+
+    private static ConcurrentHashMap<String, byte[]> fileCache = new ConcurrentHashMap<>();
+
+    private String file;
+
+    public CachedStaticFileContent(String file) {
+        this.file = file;
+    }
+
+    public CachedStaticFileContent(File file) {
+        this.file = file.getAbsolutePath();
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        byte[] bytes = fileCache.get(file);
+        if (bytes == null) {
+            bytes = IOUtils.toByteArray(new FileInputStream(file));
+            fileCache.put(file, bytes);
+        }
+        return new ByteArrayInputStream(bytes);
+    }
+}
