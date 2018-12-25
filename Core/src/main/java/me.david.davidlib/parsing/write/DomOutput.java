72f130688bfe3da61cc3ff1119a70bfa6243be4e
david <davidscandurra@gmail.com>
2018-12-25 23:54:26 +0100

    [Core] Added Dom Outout

---
+++ Core/src/main/java/me.david.davidlib/parsing/write/DomOutput.java
@@ -0,0 +1,20 @@
+package me.david.davidlib.parsing.write;
+
+import me.david.davidlib.parsing.DomSourceType;
+
+import java.io.File;
+import java.io.InputStream;
+
+public interface DomOutput {
+
+    void writeFile(File file);
+
+    String getString();
+    byte[] getBytes();
+
+    InputStream getStream();
+
+    DomSourceType getSourceType();
+
+
+}
---
+++ Core/src/main/java/me.david.davidlib/parsing/write/DomStringOutput.java
@@ -0,0 +1,50 @@
+package me.david.davidlib.parsing.write;
+
+import me.david.davidlib.io.ByteArrayInputStream;
+import me.david.davidlib.parsing.DomSourceType;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class DomStringOutput implements DomOutput {
+
+    private String string;
+
+    @Override
+    public void writeFile(File file) {
+        if (!file.getParentFile().exists()) file.mkdirs();
+        if (!file.exists()) try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            IOUtils.write(getBytes(), new FileOutputStream(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getString() {
+        return string;
+    }
+
+    @Override
+    public byte[] getBytes() {
+        return string.getBytes();
+    }
+
+    @Override
+    public InputStream getStream() {
+        return new ByteArrayInputStream(getBytes());
+    }
+
+    @Override
+    public DomSourceType getSourceType() {
+        return DomSourceType.STRING;
+    }
+}
