d5d1433cb4ef03e4cbbfc8e269214132305d458b
david <davidscandurra@gmail.com>
2018-12-27 20:53:37 +0100

    [DomParsing] Added ParsingManager implementation and created DomParsing

---
+++ DomParsing/src/main/java/de.splotycode.davidlib.domparsing/ParsingManager.java
@@ -0,0 +1,89 @@
+package de.splotycode.davidlib.domparsing;
+
+import lombok.Getter;
+import me.david.davidlib.parsing.ParsingHandle;
+import me.david.davidlib.parsing.input.DomFileInput;
+import me.david.davidlib.parsing.input.DomInput;
+import me.david.davidlib.parsing.input.DomStreamInput;
+import me.david.davidlib.parsing.input.DomUrlInput;
+import me.david.davidlib.storage.Document;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ParsingManager implements me.david.davidlib.link.ParsingManager {
+
+    @Getter private List<ParsingHandle> handles = new ArrayList<>();
+
+    @Override
+    public Document parseDocument(DomInput input, ParsingHandle handle) {
+        return handle.getParser(input).parse(input);
+    }
+
+    @Override
+    public Document parseDocument(File file) {
+        DomInput input = new DomFileInput(file);
+        ParsingHandle handle = handles.stream().filter(cHandle -> FilenameUtils.isExtension(file.getName(), cHandle.getFileTypes())).findFirst().orElseThrow(NoHandleFound::new);
+        return parseDocument(input, handle);
+    }
+
+    @Override
+    public Document parseDocument(URL url) {
+        DomUrlInput input = new DomUrlInput(url);
+        ParsingHandle handle = handles.stream().filter(cHandle -> {
+            try {
+                return ArrayUtils.contains(cHandle.getMimeTypes(), input.getConnection().getContentType());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }).findFirst().orElseThrow(NoHandleFound::new);
+        return parseDocument(input, handle);
+    }
+
+    @Override
+    public Document parseResourceFile(String file) {
+        DomInput input = new DomStreamInput(ParsingManager.class.getResourceAsStream(file));
+        ParsingHandle handle = handles.stream().filter(cHandle -> FilenameUtils.isExtension(file, cHandle.getFileTypes())).findFirst().orElseThrow(NoHandleFound::new);
+        return parseDocument(input, handle);
+    }
+
+    @Override
+    public Collection<ParsingHandle> getList() {
+        return handles;
+    }
+
+    @Override
+    public Class<ParsingHandle> getObjectClass() {
+        return ParsingHandle.class;
+    }
+
+    public static class NoHandleFound extends RuntimeException {
+
+        public NoHandleFound() {
+        }
+
+        public NoHandleFound(String s) {
+            super(s);
+        }
+
+        public NoHandleFound(String s, Throwable throwable) {
+            super(s, throwable);
+        }
+
+        public NoHandleFound(Throwable throwable) {
+            super(throwable);
+        }
+
+        public NoHandleFound(String s, Throwable throwable, boolean b, boolean b1) {
+            super(s, throwable, b, b1);
+        }
+    }
+
+}
