037b4a9029501f5bdd1579044479dcec8de66a8e
david <davidscandurra@gmail.com>
2018-12-27 13:46:45 +0100

    [Core] Added parseResourceFile to ParsingManager

--- Core/src/main/java/me.david.davidlib/link/ParsingManager.java
+++ Core/src/main/java/me.david.davidlib/link/ParsingManager.java
@@ -3,16 +3,19 @@ package me.david.davidlib.link;
 import me.david.davidlib.parsing.ParsingHandle;
 import me.david.davidlib.parsing.input.DomInput;
 import me.david.davidlib.storage.Document;
+import me.david.davidlib.utils.reflection.classregister.IListClassRegister;
 
+import java.io.File;
 import java.net.URL;
 import java.util.Collection;
 
-public interface ParsingManager {
+public interface ParsingManager extends IListClassRegister<ParsingHandle> {
 
     Collection<ParsingHandle> getHandles();
 
     Document parseDocument(DomInput input, ParsingHandle handle);
-    Document parseDocument(String file);
+    Document parseDocument(File file);
     Document parseDocument(URL url);
+    Document parseResourceFile(String file);
 
 }
