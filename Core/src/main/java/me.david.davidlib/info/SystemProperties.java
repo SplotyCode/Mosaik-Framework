9642b3f5ba32d9da06500bb4ff90fb66abe1c5e0
david <davidscandurra@gmail.com>
2018-12-28 21:31:48 +0100

    [Core] Added SystemProperties class

---
+++ Core/src/main/java/me.david.davidlib/info/SystemProperties.java
@@ -0,0 +1,56 @@
+package me.david.davidlib.info;
+
+import me.david.davidlib.cache.Cache;
+import me.david.davidlib.cache.DefaultCaches;
+
+import java.io.File;
+
+public final class SystemProperties {
+
+    private static Cache<File> userHome = DefaultCaches.getNormalValueResolverCache(cache -> getUserHome0());
+    private static Cache<String> lineSeparator = DefaultCaches.getNormalValueResolverCache(cache -> getLineSeparator0());
+    private static Cache<File> javaHome = DefaultCaches.getNormalValueResolverCache(cache -> getJavaHome0());
+    private static Cache<File> userName = DefaultCaches.getNormalValueResolverCache(cache -> getUserHome0());
+    private static Cache<File> workingDirectory = DefaultCaches.getNormalValueResolverCache(cache -> getWorkingDirectory0());
+
+
+    private static File getUserHome0() {
+        return new File(System.getProperty("user.home"));
+    }
+
+    public static File getUserHome() {
+        return userHome.getValue();
+    }
+
+    private static String getLineSeparator0() {
+        return System.getProperty("line.separator");
+    }
+
+    public static String getLineSeparator() {
+        return lineSeparator.getValue();
+    }
+
+    private static File getJavaHome0() {
+        return new File(System.getProperty("java.home"));
+    }
+
+    public static File getJavaHome() {
+        return javaHome.getValue();
+    }
+
+    private static String getUserName0() {
+        return System.getProperty("user.name");
+    }
+
+    public static File getUserName() {
+        return userName.getValue();
+    }
+
+    private static File getWorkingDirectory0() {
+        return new File(".");
+    }
+
+    public static File getWorkingDirectory() {
+        return workingDirectory.getValue();
+    }
+}
