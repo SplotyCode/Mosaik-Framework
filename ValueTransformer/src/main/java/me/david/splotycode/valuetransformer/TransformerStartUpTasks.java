6e530cde4f79da9358e74ef30b823bd25a954d1c
david <davidscandurra@gmail.com>
2019-01-21 12:32:02 +0100

    [ValueTransformer] Use ClassCollector

--- ValueTransformer/src/main/java/me/david/splotycode/valuetransformer/TransformerStartUpTasks.java
+++ ValueTransformer/src/main/java/me/david/splotycode/valuetransformer/TransformerStartUpTasks.java
@@ -5,14 +5,22 @@ import me.david.davidlib.runtime.LinkBase;
 import me.david.davidlib.runtime.startup.StartUpPriorities;
 import me.david.davidlib.runtime.startup.StartupTask;
 import me.david.davidlib.runtime.startup.envirement.StartUpEnvironmentChanger;
+import me.david.davidlib.util.AlmostBoolean;
+import me.david.davidlib.util.reflection.ClassCollector;
 
 @Priority(priority = StartUpPriorities.PRE_LINKBASE)
 public class TransformerStartUpTasks implements StartupTask {
 
+    private static ClassCollector collector = ClassCollector.newInstance()
+                                            .setAbstracation(AlmostBoolean.NO)
+                                            .setInPackage("me.david.splotycode.valuetransformer")
+                                            .setNoDisableds(true)
+                                            .setNeedAssignable(ValueTransformer.class);
+
     @Override
     public void execute(StartUpEnvironmentChanger environmentChanger) throws Exception {
         LinkBase.getInstance().registerLink(TransformerManager.LINK, new TransformerManager());
-        TransformerManager.getInstance().registerPackage("me.david.splotycode.valuetransformer");
+        TransformerManager.getInstance().registerAll(collector);
     }
 
 }
