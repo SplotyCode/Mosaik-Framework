0ddaf8f266398cee7b3c72b42a378bd759cd4134
david <davidscandurra@gmail.com>
2019-02-01 15:37:58 +0100

    [WebApi] Added ManipulateDataTest and StringManipulatorTest

---
+++ WebApi/src/test/java/io/github/splotycode/mosaik/webapi/response/content/manipulate/ManipulateDataTest.java
@@ -0,0 +1,47 @@
+package io.github.splotycode.mosaik.webapi.response.content.manipulate;
+
+import io.github.splotycode.mosaik.util.prettyprint.PrettyPrint;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ManipulateDataTest {
+
+    private static final String EXPECTED = "[pat=ManipulatePattern[\n" +
+            "    start: 5\n" +
+            "    end: 38\n" +
+            "    content:  dasd $name$ 333 $age$ \n" +
+            "    variables: [age=[ManipulateVariable[\n" +
+            "    start: 17\n" +
+            "    end: 22\n" +
+            "]], name=[ManipulateVariable[\n" +
+            "    start: 6\n" +
+            "    end: 12\n" +
+            "]]]\n" +
+            "]]";
+    final String EXPECTED_VAR = "[name1=[ManipulateVariable[\n" +
+            "    start: 24\n" +
+            "    end: 31\n" +
+            "]], name2=[ManipulateVariable[\n" +
+            "    start: 33\n" +
+            "    end: 40\n" +
+            "]], name3=[ManipulateVariable[\n" +
+            "    start: 45\n" +
+            "    end: 52\n" +
+            "]], name=[ManipulateVariable[\n" +
+            "    start: 16\n" +
+            "    end: 22\n" +
+            "]]]";
+
+    @Test
+    void testVariablePositions() {
+        StringManipulator manipulator = new StringManipulator("Hallo my friend $name$, $name1$, $name2$ and $name3$");
+        Assertions.assertEquals(EXPECTED_VAR, new PrettyPrint(manipulator.getManipulateData().getVariableMap()).prettyPrintType());
+    }
+
+    @Test
+    void testPatternPositions() {
+        StringManipulator manipulator = new StringManipulator("hallo$@pat$ dasd $name$ 333 $age$ $@@$hasdasdas");
+        Assertions.assertEquals(EXPECTED, new PrettyPrint(manipulator.getManipulateData().getPatternMap()).prettyPrintType());
+    }
+
+}
---
+++ WebApi/src/test/java/io/github/splotycode/mosaik/webapi/response/content/manipulate/StringManipulatorTest.java
@@ -0,0 +1,70 @@
+package io.github.splotycode.mosaik.webapi.response.content.manipulate;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class StringManipulatorTest {
+
+    @Test
+    void testGetResult() {
+        StringManipulator manipulator = new StringManipulator("Hallo my friend $name$");
+        manipulator.getReplacements().add(new StringManipulator.Replacement(2, 3, "no"));
+        Assertions.assertEquals(manipulator.getResult(), "Hanolo my friend $name$");
+        manipulator.reset();
+        manipulator.getReplacements().add(new StringManipulator.Replacement(4, 8, "ok"));
+        Assertions.assertEquals(manipulator.getResult(), "Hallok friend $name$");
+        manipulator.reset();
+        manipulator.getReplacements().add(new StringManipulator.Replacement(2, 3, "no"));
+        manipulator.getReplacements().add(new StringManipulator.Replacement(4, 8, "ok"));
+        Assertions.assertEquals(manipulator.getResult(), "Hanolok friend $name$");
+    }
+
+    @Test
+    void testVariables() {
+        StringManipulator manipulator = new StringManipulator("Hallo my friend $name$");
+        manipulator.variable("name", 33);
+        Assertions.assertEquals(manipulator.getResult(), "Hallo my friend 33");
+    }
+
+    @Test
+    void testMultipleVariables() {
+        StringManipulator manipulator = new StringManipulator("Hallo my friend $name$, $name1$, $name2$ and $name3$");
+        manipulator.variable("name", "hallo");
+        manipulator.variable("name1", "duuu222222");
+        manipulator.variable("name2", "anntonn");
+        manipulator.variable("name3", "ahllooouuuuuu");
+        Assertions.assertEquals(manipulator.getResult(), "Hallo my friend hallo, duuu222222, anntonn and ahllooouuuuuu");
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    class Pat {
+        String name = "david";
+        int age = 14;
+    }
+
+    @Test
+    void testPatterns() {
+        StringManipulator manipulator = new StringManipulator("hallo$@pat$ dasd $name$ 333 $age$ $@@$hasdasdas");
+        manipulator.pattern(new Pat());
+        Assertions.assertEquals(manipulator.getResult(), "hallo dasd david 333 14 hasdasdas");
+    }
+
+    @Test
+    void testPatternList() {
+        StringManipulator manipulator = new StringManipulator("hallo$@pat$ Name: $name$ <br> Age: $age$ <br><br> $@@$hasdasdas");
+        List<Pat> data = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            data.add(new Pat("david", i + 10));
+        }
+        manipulator.pattern(data);
+        Assertions.assertEquals(manipulator.getResult(), "hallo Name: david <br> Age: 15 <br><br>  Name: david <br> Age: 20 <br><br>  Name: david <br> Age: 13 <br><br>  Name: david <br> Age: 21 <br><br>  Name: david <br> Age: 14 <br><br>  Name: david <br> Age: 19 <br><br>  Name: david <br> Age: 10 <br><br>  Name: david <br> Age: 12 <br><br>  Name: david <br> Age: 18 <br><br>  Name: david <br> Age: 17 <br><br>  Name: david <br> Age: 16 <br><br>  Name: david <br> Age: 11 <br><br> hasdasdas");
+    }
+
+
+}
