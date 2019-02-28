0ac2da003abf7fc1e9728c2e119dceb2cb8adc14
david <davidscandurra@gmail.com>
2019-02-28 11:07:50 +0100

    [Util] IModule#getAllDependencies() IModule#isLoaded

--- Util/src/main/java/io/github/splotycode/mosaik/util/reflection/modules/IModule.java
+++ Util/src/main/java/io/github/splotycode/mosaik/util/reflection/modules/IModule.java
@@ -1,11 +1,39 @@
 package io.github.splotycode.mosaik.util.reflection.modules;
 
+import io.github.splotycode.mosaik.util.reflection.ReflectionUtil;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 public interface IModule {
 
-    IModule[] getDependecies();
+    IModule[] getDependencies();
 
     String getDisplayName();
 
     String[] loadChecker();
+
+    default Collection<IModule> getAllDependencies() {
+        return getAllDependencies(new ArrayList<>(), this);
+    }
+
+    default Collection<IModule> getAllDependencies(Collection<IModule> list, IModule base) {
+        for (IModule module : base.getDependencies()) {
+            list.add(module);
+            list = getAllDependencies(list, module);
+        }
+        return list;
+    }
+
+    default boolean isLoaded() {
+        for (IModule dependency : getAllDependencies()) {
+            for (String checker : dependency.loadChecker()) {
+                if (!ReflectionUtil.clazzExists(checker)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
 }
--- Util/src/main/java/io/github/splotycode/mosaik/util/reflection/modules/MosaikModule.java
+++ Util/src/main/java/io/github/splotycode/mosaik/util/reflection/modules/MosaikModule.java
@@ -43,7 +43,7 @@ public enum MosaikModule implements IModule {
     }
 
     @Override
-    public IModule[] getDependecies() {
+    public IModule[] getDependencies() {
         return dependencies;
     }
 
