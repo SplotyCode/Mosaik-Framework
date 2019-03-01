ad285a1c71e487432c68d46fe67600eee701a204
david <davidscandurra@gmail.com>
2019-03-02 00:29:03 +0100

    [Util] StringUtil: Added Method with separator & EnumUtil#toDisplayName(): Use the new camelCase() method

--- Util/src/main/java/io/github/splotycode/mosaik/util/EnumUtil.java
+++ Util/src/main/java/io/github/splotycode/mosaik/util/EnumUtil.java
@@ -3,7 +3,7 @@ package io.github.splotycode.mosaik.util;
 public final class EnumUtil {
 
     public static String toDisplayName(Enum enumm) {
-        return StringUtil.camelCase(enumm.name().replace('_', '-'));
+        return StringUtil.camelCase(enumm.name().replace('_', '-'), "-");
     }
 
 }
