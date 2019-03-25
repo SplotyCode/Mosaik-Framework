872e9554afb15b790c03811ca89d31b4ed8131a5
david <davidscandurra@gmail.com>
2019-03-25 22:23:59 +0100

    [Netty] (BindListener & BoundListener): Add Component Argument

--- Netty/src/main/java/io/github/splotycode/mosaik/netty/component/listener/BindListener.java
+++ Netty/src/main/java/io/github/splotycode/mosaik/netty/component/listener/BindListener.java
@@ -1,10 +1,11 @@
 package io.github.splotycode.mosaik.netty.component.listener;
 
+import io.github.splotycode.mosaik.netty.component.NetworkComponent;
 import io.github.splotycode.mosaik.util.listener.Listener;
 import io.netty.bootstrap.AbstractBootstrap;
 
 public interface BindListener<B extends AbstractBootstrap> extends Listener {
 
-    void bind(B bootstrap);
+    void bind(NetworkComponent component, B bootstrap);
 
 }
--- Netty/src/main/java/io/github/splotycode/mosaik/netty/component/listener/BoundListener.java
+++ Netty/src/main/java/io/github/splotycode/mosaik/netty/component/listener/BoundListener.java
@@ -1,10 +1,11 @@
 package io.github.splotycode.mosaik.netty.component.listener;
 
+import io.github.splotycode.mosaik.netty.component.NetworkComponent;
 import io.github.splotycode.mosaik.util.listener.Listener;
 import io.netty.channel.ChannelFuture;
 
 public interface BoundListener extends Listener {
 
-    void bound(ChannelFuture channel);
+    void bound(NetworkComponent component, ChannelFuture future);
 
 }
