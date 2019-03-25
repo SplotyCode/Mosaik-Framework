b6148673a39c7ec4dd4a7c30af1962aee2d1789d
david <davidscandurra@gmail.com>
2019-03-25 22:18:40 +0100

    [Netty] ChannelListener -> StatusListener

--- Netty/src/main/java/io/github/splotycode/mosaik/netty/component/listener/ChannelListener.java
+++
@@ -1,4 +0,0 @@
-package io.github.splotycode.mosaik.netty.component.listener;
-
-public interface ChannelListener extends BindListener, BoundListener, UnBoundListener {
-}
---
+++ Netty/src/main/java/io/github/splotycode/mosaik/netty/component/listener/StatusListener.java
@@ -0,0 +1,4 @@
+package io.github.splotycode.mosaik.netty.component.listener;
+
+public interface StatusListener extends BindListener, BoundListener, UnBoundListener {
+}
