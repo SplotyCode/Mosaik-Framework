cef115619df621fc12c325ef2ce5c6af3e30339a
david <davidscandurra@gmail.com>
2019-03-25 22:18:01 +0100

    [Netty] ReconnectListener: Logging and diff. timers for bound and unbound

--- Netty/src/main/java/io/github/splotycode/mosaik/netty/component/listener/standart/ReconnectListener.java
+++ Netty/src/main/java/io/github/splotycode/mosaik/netty/component/listener/standart/ReconnectListener.java
@@ -1,38 +1,64 @@
 package io.github.splotycode.mosaik.netty.component.listener.standart;
 
 import io.github.splotycode.mosaik.netty.component.NetworkComponent;
+import io.github.splotycode.mosaik.netty.component.listener.BoundListener;
 import io.github.splotycode.mosaik.netty.component.listener.UnBoundListener;
+import io.github.splotycode.mosaik.util.logger.Logger;
 import io.netty.channel.ChannelFuture;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
-public class ReconnectListener implements UnBoundListener {
+public class ReconnectListener implements UnBoundListener, BoundListener {
 
-    private long reconnectMultiplier;
+    private static Logger logger = Logger.getInstance(ReconnectListener.class);
+
+    private float reconnectMultiplier;
+
     private long reconnectMultiplierStart;
     private long reconnectMultiplierMax;
 
-    private long currentReconnect = reconnectMultiplierStart;
+    private volatile AtomicLong currentReconnectBound = new AtomicLong();
+    private volatile AtomicLong currentReconnectUnBound = new AtomicLong();
 
     public ReconnectListener() {
         this(2, 4, 16);
     }
 
-    public ReconnectListener(long reconnectMultiplier, long reconnectMultiplierStart, long reconnectMultiplierMax) {
+    public ReconnectListener(float reconnectMultiplier, long reconnectMultiplierStart, long reconnectMultiplierMax) {
         this.reconnectMultiplier = reconnectMultiplier;
         this.reconnectMultiplierStart = reconnectMultiplierStart;
         this.reconnectMultiplierMax = reconnectMultiplierMax;
+        currentReconnectBound.set(reconnectMultiplierStart);
+        currentReconnectUnBound.set(reconnectMultiplierStart);
     }
 
     @Override
     public void unBound(NetworkComponent component, ChannelFuture future) {
+        handleReconnect(component, future, false);
+    }
+
+    @Override
+    public void bound(NetworkComponent component, ChannelFuture future) {
+        handleReconnect(component, future, true);
+    }
+
+    private void handleReconnect(NetworkComponent component, ChannelFuture future, boolean bound) {
+        AtomicLong atomicDelay = bound ? currentReconnectBound : currentReconnectUnBound;
         if (!future.isSuccess()) {
-            future.channel().close();
-            currentReconnect = Math.min(reconnectMultiplierMax, currentReconnect *= reconnectMultiplier);
-            future.channel().eventLoop().schedule((Runnable) component::bind, currentReconnect, TimeUnit.SECONDS);
+            atomicDelay.getAndUpdate(reconnectDelay -> {
+                logger.info("Reconnecting " + component.getClass().getSimpleName() + " (Triggered By: " + (bound ? "Bound" : "Unbound") + ") in " + reconnectDelay + " seconds...");
+                future.channel().eventLoop().schedule(() -> {
+                    logger.info("Reconnection " + component.getClass().getSimpleName() + " now");
+                    component.bind(true);
+                    future.channel().eventLoop().shutdownGracefully();
+                }, reconnectDelay, TimeUnit.SECONDS);
+                return Math.round(Math.min(reconnectMultiplierMax, reconnectDelay * reconnectMultiplier));
+            });
         } else {
-            currentReconnect = reconnectMultiplierStart;
+            atomicDelay.set(reconnectMultiplierStart);
         }
     }
+
 
 }
