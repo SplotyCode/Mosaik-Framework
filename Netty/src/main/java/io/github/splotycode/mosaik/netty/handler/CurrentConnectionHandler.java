ba8a7f752b5c3d936af1c3dad7b294eb43c4d4cc
david <davidscandurra@gmail.com>
2019-04-03 21:13:07 +0200

    [Netty] CurrentConnectionHandler: Typo, Getters

--- Netty/src/main/java/io/github/splotycode/mosaik/netty/handler/CurrentConnectionHandler.java
+++ Netty/src/main/java/io/github/splotycode/mosaik/netty/handler/CurrentConnectionHandler.java
@@ -10,7 +10,23 @@ import java.util.concurrent.atomic.AtomicLong;
 public class CurrentConnectionHandler extends ChannelInboundHandlerAdapter {
 
     private AtomicLong connections = new AtomicLong();
-    private AtomicLong activConnections = new AtomicLong();
+    private AtomicLong activeConnections = new AtomicLong();
+
+    public long getConnections() {
+        return connections.get();
+    }
+
+    public long getActiveConnections() {
+        return activeConnections.get();
+    }
+
+    public AtomicLong getConnectionsAtomic() {
+        return connections;
+    }
+
+    public AtomicLong getActiveConnectionsAtomic() {
+        return activeConnections;
+    }
 
     @Override
     public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
@@ -27,12 +43,12 @@ public class CurrentConnectionHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         super.channelActive(ctx);
-        activConnections.incrementAndGet();
+        activeConnections.incrementAndGet();
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
         super.channelInactive(ctx);
-        activConnections.decrementAndGet();
+        activeConnections.decrementAndGet();
     }
 }
