ba668761425a53112251321193907d2230a5aa18
david <davidscandurra@gmail.com>
2019-04-03 21:12:21 +0200

    [Util] Added DefaultCaches#getTimeCache(Validator, Long)

--- Util/src/main/java/io/github/splotycode/mosaik/util/cache/DefaultCaches.java
+++ Util/src/main/java/io/github/splotycode/mosaik/util/cache/DefaultCaches.java
@@ -1,6 +1,7 @@
 package io.github.splotycode.mosaik.util.cache;
 
 import io.github.splotycode.mosaik.util.cache.complex.resolver.CacheValueResolver;
+import io.github.splotycode.mosaik.util.cache.complex.validator.TimeValidator;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,10 @@ public final class DefaultCaches {
 
     public static <T> Cache<T> getNormalValueResolverCache(CacheValueResolver<T> valueResolver) {
         return getNormalValueResolverBuilder(valueResolver).build();
+    }
+
+    public static <T> Cache<T> getTimeCache(CacheValueResolver<T> valueResolver, long maxAge) {
+        return getNormalValueResolverBuilder(valueResolver).setValidator(new TimeValidator<>(maxAge)).build();
     }
 
     public static <T> CacheBuilder.NormalBuilder<T> getNormalValueResolverBuilder(CacheValueResolver<T> valueResolver) {
