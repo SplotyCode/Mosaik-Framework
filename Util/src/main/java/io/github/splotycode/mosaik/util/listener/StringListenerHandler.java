d0f93225f40e4dd6346c4df6f70194e962e02a6e
david <davidscandurra@gmail.com>
2019-04-09 21:13:11 +0200

    [Util] Added StringListenerHandler

---
+++ Util/src/main/java/io/github/splotycode/mosaik/util/listener/StringListenerHandler.java
@@ -0,0 +1,44 @@
+package io.github.splotycode.mosaik.util.listener;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class StringListenerHandler<L extends Listener> implements ListenerHandler<L> {
+
+    private Multimap<String, L> listeners = new HashMultimap<>();
+
+    public void addListener(String prefix, L listener) {
+        listeners.put(prefix, listener);
+    }
+
+    public void addListener(Class clazz, L listener) {
+        listeners.put(clazz.getName(), listener);
+    }
+
+    @Override
+    public void addListener(L listener) {
+        addListener("*", listener);
+    }
+
+    @Override
+    public Collection<L> getListeners() {
+        return listeners.values();
+    }
+
+    public void call(String prefix, Consumer<L> consumer) {
+        for (Map.Entry<String, L> listener : listeners.entries()) {
+            if (listener.getKey().equals("*") || listener.getKey().startsWith(prefix)) {
+                consumer.accept(listener.getValue());
+            }
+        }
+    }
+
+    @Override
+    public void call(Consumer<L> consumer) {
+        getListeners().forEach(consumer);
+    }
+}
