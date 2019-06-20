7a5396ea4dfd9273457c63c479c145cfde09cae6
david <davidscandurra@gmail.com>
2019-06-20 18:42:27 +0200

    [Util] Added MessageContext

---
+++ Util/src/main/java/io/github/splotycode/mosaik/util/i18n/MessageContext.java
@@ -0,0 +1,57 @@
+package io.github.splotycode.mosaik.util.i18n;
+
+import lombok.EqualsAndHashCode;
+import lombok.Setter;
+
+import java.util.Map;
+
+@EqualsAndHashCode
+public class MessageContext {
+
+    protected I18N translator;
+    protected Map<String, String> replacements;
+    @Setter protected String prefix;
+    @Setter protected boolean allLinePrefix;
+
+    public MessageContext(I18N translator, String prefix, boolean allLinePrefix) {
+        this.translator = translator;
+        this.prefix = prefix;
+        this.allLinePrefix = allLinePrefix;
+    }
+
+    protected RuntimeException getErrorException(String message) {
+        return new RuntimeException(message);
+    }
+
+    public void throwErrorRaw(String text) {
+        throw getErrorException(text);
+    }
+
+    public void throwError(String key, Object... objects) {
+        throwErrorRaw(translate(key, objects));
+    }
+
+    public String transformRaw(String text) {
+        for (Map.Entry<String, String> replacement : replacements.entrySet()) {
+            text = text.replace(replacement.getKey(), replacement.getValue());
+        }
+        if (allLinePrefix) {
+            StringBuilder builder = new StringBuilder();
+            for (String line : text.split("\n")) {
+                builder.append(prefix).append(line);
+            }
+            return builder.toString();
+        }
+        return prefix + text;
+    }
+
+    public MessageContext addReplacement(String key, String value) {
+        replacements.put(key, value);
+        return this;
+    }
+
+    public String translate(String key, Object... objects) {
+        return transformRaw(translator.get(key, objects));
+    }
+
+}
