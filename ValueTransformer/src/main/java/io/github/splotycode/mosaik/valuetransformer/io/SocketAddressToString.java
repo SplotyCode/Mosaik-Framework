4933f4bea841e4964fd923068b3d3d89db5bcdf4
david <davidscandurra@gmail.com>
2019-06-20 18:33:40 +0200

    [ValueTransformer] SocketAddressToString: Use NetworkUtil for better INetSocketAddress support

--- ValueTransformer/src/main/java/io/github/splotycode/mosaik/valuetransformer/io/SocketAddressToString.java
+++ ValueTransformer/src/main/java/io/github/splotycode/mosaik/valuetransformer/io/SocketAddressToString.java
@@ -1,6 +1,6 @@
 package io.github.splotycode.mosaik.valuetransformer.io;
 
-import io.github.splotycode.mosaik.util.StringUtil;
+import io.github.splotycode.mosaik.util.NetworkUtil;
 import io.github.splotycode.mosaik.util.ValueTransformer;
 import io.github.splotycode.mosaik.util.datafactory.DataFactory;
 
@@ -9,6 +9,6 @@ import java.net.SocketAddress;
 public class SocketAddressToString extends ValueTransformer<SocketAddress, String> {
     @Override
     public String transform(SocketAddress input, DataFactory info) throws Exception {
-        return StringUtil.getLastSplit(input.toString(), "/");
+        return NetworkUtil.extractHost(input);
     }
 }
