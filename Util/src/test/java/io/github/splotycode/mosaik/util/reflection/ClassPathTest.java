143f97bf931559865777f8269202132bb949e517
david <davidscandurra@gmail.com>
2019-07-22 15:37:31 +0200

    QuickFix: Fixed ClassPathTest on travis #4

--- Util/src/test/java/io/github/splotycode/mosaik/util/reflection/ClassPathTest.java
+++ Util/src/test/java/io/github/splotycode/mosaik/util/reflection/ClassPathTest.java
@@ -7,15 +7,17 @@ public class ClassPathTest {
     @Test(expected = RuntimeException.class)
     public void test() {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        ClassPath path = new ClassPath(getClass().getClassLoader());
         if (classLoader != null) {
-            path.classes(s -> {
-                throw new RuntimeException();
-            });
+            test(new ClassPath(classLoader));
         }
-        classLoader = ClassPathTest.class.getClassLoader();
-        path = new ClassPath(classLoader);
+        test(new ClassPath(ClassPathTest.class.getClassLoader()));
+    }
+
+    private void test(ClassPath path) {
         path.classes(s -> {
+            throw new RuntimeException();
+        });
+        path.resources(s -> {
             throw new RuntimeException();
         });
     }
