201ec6832c9a67f326346a491c55e7c55a2c93d0
SplotyCode <DzTTn9DYNW%X9$c!@&q8^a23@2EcWSJ4>
2019-07-22 03:44:58 +0200

    [ValueTransformer] Added possibility for translations (for spigot)
    Signed-off-by: SplotyCode <DzTTn9DYNW%X9$c!@&q8^a23@2EcWSJ4>

--- ValueTransformer/src/main/java/io/github/splotycode/mosaik/valuetransformer/CommonData.java
+++ ValueTransformer/src/main/java/io/github/splotycode/mosaik/valuetransformer/CommonData.java
@@ -1,6 +1,7 @@
 package io.github.splotycode.mosaik.valuetransformer;
 
 import io.github.splotycode.mosaik.util.datafactory.DataKey;
+import io.github.splotycode.mosaik.util.i18n.I18N;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -13,5 +14,9 @@ public final class CommonData {
     public static final DataKey<Class> RESULT = new DataKey<>("result");
     public static final DataKey<Charset> CHARSET = new DataKey<>("charset");
     public static final DataKey<File> BASE_PATH = new DataKey<>("base_path");
+
+    public static final DataKey<I18N> I18N = new DataKey<>("translation.i18n");
+
+    public static final DataKey<String> TRANSLATION_PREFIX = new DataKey<>("translation.prefix");
 
 }
