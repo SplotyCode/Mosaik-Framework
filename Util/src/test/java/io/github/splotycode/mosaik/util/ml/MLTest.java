a546b6e2b7f8dd62f84db39b244b45bfcc6825d0
david <davidscandurra@gmail.com>
2019-07-22 14:49:47 +0200

    QuickFix: Avoid long log files due to Junit MLTest

--- Util/src/test/java/io/github/splotycode/mosaik/util/ml/MLTest.java
+++ Util/src/test/java/io/github/splotycode/mosaik/util/ml/MLTest.java
@@ -3,7 +3,6 @@ package io.github.splotycode.mosaik.util.ml;
 import org.junit.Test;
 import org.junit.jupiter.api.Disabled;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class MLTest {
@@ -18,7 +17,7 @@ public class MLTest {
             int[] arr = xorInputs[i % 4];
             double[] inputs = { arr[0], arr[1] };
             double[] outputs = { arr[0] ^ arr[1] };
-            System.out.println(Arrays.toString(inputs) + " " + Arrays.toString(outputs));
+            //System.out.println(Arrays.toString(inputs) + " " + Arrays.toString(outputs));
         }
     }
 
