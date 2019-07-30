fcd95adc2015f05dfe2817e2ee2fbf3c5a45cffb
david <davidscandurra@gmail.com>
2019-07-30 22:23:30 +0200

    [ValueTransformer] StringToEnum: Fixed constant string

--- ValueTransformer/src/main/java/io/github/splotycode/mosaik/valuetransformer/other/StringToEnum.java
+++ ValueTransformer/src/main/java/io/github/splotycode/mosaik/valuetransformer/other/StringToEnum.java
@@ -1,11 +1,13 @@
 package io.github.splotycode.mosaik.valuetransformer.other;
 
+import io.github.splotycode.mosaik.util.StringUtil;
 import io.github.splotycode.mosaik.util.ValueTransformer;
 import io.github.splotycode.mosaik.util.datafactory.DataFactory;
 import io.github.splotycode.mosaik.valuetransformer.CommonData;
 import io.github.splotycode.mosaik.valuetransformer.TransformException;
 
 import java.lang.reflect.Method;
+import java.util.Objects;
 
 public class StringToEnum extends ValueTransformer<String, Enum> {
 
@@ -19,7 +21,8 @@ public class StringToEnum extends ValueTransformer<String, Enum> {
             Method values = clazz.getMethod("values");
             values.setAccessible(true);
             Object[] constants = (Object[]) values.invoke(null);
-            throw TransformException.createTranslated(info, "string_to_enum", "{0} is not a valid enum constant! Valid constants: {1}", ex, input, input, constants);
+            String constantString = StringUtil.join(constants, Objects::toString);
+            throw TransformException.createTranslated(info, "string_to_enum", "{0} is not a valid enum constant! Valid constants: {1}", ex, input, input, constantString);
         }
     }
 
