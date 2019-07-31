7afbf78e52e2b47048af5aa603d4e5a87d4c1710
david <davidscandurra@gmail.com>
2019-07-31 13:29:12 +0200

    [Util] Added CodecUtil#decodeHex

--- Util/src/main/java/io/github/splotycode/mosaik/util/CodecUtil.java
+++ Util/src/main/java/io/github/splotycode/mosaik/util/CodecUtil.java
@@ -59,12 +59,22 @@ public final class CodecUtil {
      */
     public static String bytesToHex(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
-        for ( int j = 0; j < bytes.length; j++ ) {
+        for (int j = 0; j < bytes.length; j++) {
             int v = bytes[j] & 0xFF;
             hexChars[j * 2] = hexArray[v >>> 4];
             hexChars[j * 2 + 1] = hexArray[v & 0x0F];
         }
         return new String(hexChars);
+    }
+
+    public static byte[] decodeHex(String encoded) {
+        byte[] out = new byte[encoded.length() >> 1];
+        for (int i = 0, j = 0; j < encoded.length(); i++, j++) {
+            out[i] = (byte) (((Character.digit(encoded.charAt(j), 16) << 4) |
+                               Character.digit(encoded.charAt(++j), 16))
+                    & 0xFF);
+        }
+        return out;
     }
 
     /*
