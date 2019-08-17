935d916cee470c16faac01fee087f1098692dd5d
david <davidscandurra@gmail.com>
2019-08-17 16:06:36 +0200

    [Runtime] Avoid exceptions when Application types don't have init methods

--- Runtime/src/main/java/io/github/splotycode/mosaik/runtime/application/ApplicationType.java
+++ Runtime/src/main/java/io/github/splotycode/mosaik/runtime/application/ApplicationType.java
@@ -1,6 +1,13 @@
 package io.github.splotycode.mosaik.runtime.application;
 
+import io.github.splotycode.mosaik.runtime.startup.BootContext;
+
 public interface ApplicationType extends IApplication {
 
+    /*
+     * Do not override this method.
+     * Change ApplicationType to you application type
+     */
+    default void initType(BootContext context, ApplicationType type) {}
 
 }
