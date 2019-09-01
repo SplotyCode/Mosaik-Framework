096cd97710a72a9d2af9fce329a91b88c8fa91d8
david <davidscandurra@gmail.com>
2019-09-01 20:43:48 +0200

    [Networking] Typos

--- Networking/src/main/java/io/github/splotycode/mosaik/networking/store/HashMethod.java
+++ Networking/src/main/java/io/github/splotycode/mosaik/networking/store/HashMethod.java
@@ -2,19 +2,19 @@ package io.github.splotycode.mosaik.networking.store;
 
 public enum HashMethod {
 
-    MODO_STANDART{
+    MODULO_STANDARD {
         @Override
         public int hash(int hashcode, int buckets) {
             return Math.abs(hashcode % buckets);
         }
     },
-    JDK{
+    JDK {
         @Override
         public int hash(int hashcode, int buckets) {
             return buckets & (hashcode ^ hashcode >>> 16);
         }
     },
-    MODO_JDK{
+    MODULO_JDK {
         @Override
         public int hash(int hashcode, int buckets) {
             hashcode ^= (hashcode >>> 20) ^ (hashcode >>> 12);
