ed34e2cb093f14fde9b21e8b2b1689787e2f10df
david <davidscandurra@gmail.com>
2019-12-25 10:32:32 +0100

    [DomParsing] Added JsonHandle

---
+++ DomParsing-Impl/src/main/java/io/github/splotycode/mosaik/domparsingimpl/formats/json/JsonHandle.java
@@ -0,0 +1,28 @@
+package io.github.splotycode.mosaik.domparsingimpl.formats.json;
+
+import io.github.splotycode.mosaik.domparsing.parsing.DomParser;
+import io.github.splotycode.mosaik.domparsing.parsing.ParsingHandle;
+import io.github.splotycode.mosaik.domparsing.parsing.input.DomInput;
+import io.github.splotycode.mosaik.domparsing.writing.DomWriter;
+
+public class JsonHandle implements ParsingHandle {
+    @Override
+    public String[] getFileTypes() {
+        return new String[] {"json"};
+    }
+
+    @Override
+    public String[] getMimeTypes() {
+        return new String[] {"application/json"};
+    }
+
+    @Override
+    public DomParser getParser(DomInput input) {
+        return null;
+    }
+
+    @Override
+    public DomWriter getWriter() {
+        return null;
+    }
+}
