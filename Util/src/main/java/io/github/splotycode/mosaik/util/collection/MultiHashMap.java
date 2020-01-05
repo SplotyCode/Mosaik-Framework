3ed5bcb11dfc1d6e76e07ecafd38666184b7fff8
david <davidscandurra@gmail.com>
2020-01-05 15:57:16 +0100

    [Util] MultiHashMap: Added getAllValues() and totalLength()

--- Util/src/main/java/io/github/splotycode/mosaik/util/collection/MultiHashMap.java
+++ Util/src/main/java/io/github/splotycode/mosaik/util/collection/MultiHashMap.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 public class MultiHashMap<K, V> extends HashMap<K, ArrayList<V>> {
 
+    private Values values;
+
     public MultiHashMap() {
     }
 
@@ -11,8 +13,38 @@ public class MultiHashMap<K, V> extends HashMap<K, ArrayList<V>> {
         super(initialCapacity);
     }
 
+    public MultiHashMap(int initialCapacity, float loadFactor) {
+        super(initialCapacity, loadFactor);
+    }
+
     public MultiHashMap(Map<? extends K, ? extends ArrayList<V>> m) {
         super(m);
+    }
+
+    private class Values extends AbstractCollection<V> {
+
+        @Override
+        public Iterator<V> iterator() {
+            return allValues();
+        }
+
+        @Override
+        public int size() {
+            return totalLength();
+        }
+
+        @Override
+        public void clear() {
+            MultiHashMap.this.clear();
+        }
+
+    }
+
+    public Collection<V> getAllValues() {
+        if (values == null) {
+            values = new Values();
+        }
+        return values;
     }
 
     public Iterator<V> allValues() {
@@ -36,8 +68,12 @@ public class MultiHashMap<K, V> extends HashMap<K, ArrayList<V>> {
         };
     }
 
-    public MultiHashMap(int initialCapacity, float loadFactor) {
-        super(initialCapacity, loadFactor);
+    public int totalLength() {
+        int length = 0;
+        for (ArrayList<V> list : this.values()) {
+            length += list.size();
+        }
+        return length;
     }
 
     public void addToList(K key, V value) {
