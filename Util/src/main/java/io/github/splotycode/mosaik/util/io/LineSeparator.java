cbe3870e2377a6c616f64acca2be6a4ea689b98b
david <davidscandurra@gmail.com>
2020-07-26 21:05:24 +0200

    [Util] LineSeparator: Added getChars() and streamLines(Reader)
    Signed-off-by: david <davidscandurra@gmail.com>

--- Util/src/main/java/io/github/splotycode/mosaik/util/io/LineSeparator.java
+++ Util/src/main/java/io/github/splotycode/mosaik/util/io/LineSeparator.java
@@ -1,9 +1,16 @@
 package io.github.splotycode.mosaik.util.io;
 
+import io.github.splotycode.mosaik.util.collection.SimpleIterator;
 import io.github.splotycode.mosaik.util.info.OperationSystem;
 import lombok.Getter;
+import lombok.SneakyThrows;
 
-import java.nio.charset.StandardCharsets;
+import java.io.Reader;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @Getter
 public enum LineSeparator {
@@ -16,18 +23,47 @@ public enum LineSeparator {
         return OperationSystem.current().getSeparator();
     }
 
+    private final String separator;
+    private char[] chars;
+
     LineSeparator(String separator) {
         this.separator = separator;
+        chars = separator.toCharArray();
     }
 
-    private String separator;
-    private byte[] bytes;
+    public Stream<String> streamLines(Reader reader) {
+        char[] separator = getChars();
+        StringBuilder builder = new StringBuilder();
+        Iterator<String> iter = new SimpleIterator<String>() {
+            @Override
+            @SneakyThrows
+            protected boolean provideNext() {
+                int currentChar;
+                int separatorIndex = 0;
+                while ((currentChar = reader.read()) != -1) {
+                    if (currentChar == separator[separatorIndex]) {
+                        separatorIndex++;
+                        if (separatorIndex == separator.length) {
+                            builder.setLength(builder.length() - separator.length + 1);
+                            next = builder.toString();
+                            builder.setLength(0);
+                            return true;
+                        }
+                    } else {
+                        separatorIndex = 0;
+                    }
+                    builder.append((char) currentChar);
+                }
+                if (builder.length() != 0) {
+                    next = builder.toString();
+                    builder.setLength(0);
+                    return true;
+                }
+                return false;
+            }
+        };
 
-    public byte[] getBytes() {
-        if (bytes == null) {
-            bytes = separator.getBytes(StandardCharsets.UTF_8);
-        }
-        return bytes;
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(
+                iter, Spliterator.IMMUTABLE | Spliterator.ORDERED | Spliterator.NONNULL), false);
     }
-
 }
