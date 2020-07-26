f4eaa6d03968245f558371f4ec3106a1826dfd94
david <davidscandurra@gmail.com>
2020-07-26 21:01:32 +0200

    [Util] SimpleIterator: Allow to set start element and made usage easier when elements are not allowed to be null
    Signed-off-by: david <davidscandurra@gmail.com>

--- Util/src/main/java/io/github/splotycode/mosaik/util/collection/SimpleIterator.java
+++ Util/src/main/java/io/github/splotycode/mosaik/util/collection/SimpleIterator.java
@@ -1,12 +1,36 @@
 package io.github.splotycode.mosaik.util.collection;
 
+import lombok.NoArgsConstructor;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+@NoArgsConstructor
 public abstract class SimpleIterator<T> implements Iterator<T> {
+
+    @NoArgsConstructor
+    public static abstract class NotNullSimpleIterator<T> extends SimpleIterator<T> {
+
+        public NotNullSimpleIterator(T next) {
+            super(next);
+        }
+
+        protected abstract T provideNext0();
+
+        @Override
+        protected boolean provideNext() {
+            next = provideNext0();
+            return next != null;
+        }
+    }
 
     protected T next;
     protected boolean hasElement;
+
+    public SimpleIterator(T next) {
+        this.next = next;
+        this.hasElement = true;
+    }
 
     protected abstract boolean provideNext();
 
@@ -30,5 +54,4 @@ public abstract class SimpleIterator<T> implements Iterator<T> {
             hasElement = false;
         }
     }
-
 }
