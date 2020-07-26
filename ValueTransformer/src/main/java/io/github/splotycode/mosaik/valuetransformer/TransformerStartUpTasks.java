888ff45a34326d94139d5f7b403e328f73e5ffac
david <davidscandurra@gmail.com>
2020-07-26 21:48:34 +0200

    [ValueTransformer] Use new collector and classpath api
    Signed-off-by: david <davidscandurra@gmail.com>

--- ValueTransformer/src/main/java/io/github/splotycode/mosaik/valuetransformer/TransformerStartUpTasks.java
+++ ValueTransformer/src/main/java/io/github/splotycode/mosaik/valuetransformer/TransformerStartUpTasks.java
@@ -2,16 +2,16 @@ package io.github.splotycode.mosaik.valuetransformer;
 
 import io.github.splotycode.mosaik.annotations.priority.Priority;
 import io.github.splotycode.mosaik.runtime.LinkBase;
+import io.github.splotycode.mosaik.runtime.Runtime;
 import io.github.splotycode.mosaik.runtime.startup.StartUpPriorities;
 import io.github.splotycode.mosaik.runtime.startup.StartupTask;
 import io.github.splotycode.mosaik.runtime.startup.environment.StartUpEnvironmentChanger;
 import io.github.splotycode.mosaik.util.AlmostBoolean;
 import io.github.splotycode.mosaik.util.ValueTransformer;
-import io.github.splotycode.mosaik.util.reflection.ClassCollector;
+import io.github.splotycode.mosaik.util.reflection.collector.ClassCollector;
 
 @Priority(priority = StartUpPriorities.PRE_LINKBASE)
 public class TransformerStartUpTasks implements StartupTask {
-
     private static ClassCollector collector = ClassCollector.newInstance()
                                             .setAbstracation(AlmostBoolean.NO)
                                             .setInPackage("io.github.splotycode.mosaik.valuetransformer")
@@ -21,7 +21,6 @@ public class TransformerStartUpTasks implements StartupTask {
     @Override
     public void execute(StartUpEnvironmentChanger environmentChanger) throws Exception {
         LinkBase.getInstance().registerLink(TransformerManager.LINK, new TransformerManager());
-        TransformerManager.getInstance().registerAll(collector);
+        TransformerManager.getInstance().registerAll(collector, Runtime.getRuntime().getGlobalClassPath());
     }
-
 }
